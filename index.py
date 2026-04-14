4fefe71a3072bccc0cf34659e21a3509e8fd92cb
sifihub <sifihub@proton.me>
2026-04-14 16:55:14 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import undetected_chromedriver as uc
+
+options = Options()
+options = uc.ChromeOptions()
+options.binary_location = "/home/dhruv/Downloads/ungoogled-chromium-145.0.7632.159-1-x86_64.AppImage"
+options.add_argument("--no-sandbox")
+options.add_argument("--user-data-dir=/home/dhruv/Desktop/chromium")
+options.add_argument("--profile-directory=Default")
+options.add_argument("--password-store=basic")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1345x610")
+
+
+
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1354, 610)
+actions = ActionChains(driver)
